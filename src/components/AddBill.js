5e74f401361697639892e9037fde76bad3618698
gustavohdev <g_a93@outlook.com>
2021-11-17 10:52:07 -0300

    adding bils

--- src/components/AddBill.js
+++ src/components/AddBill.js
@@ -1,5 +1,65 @@
-const AddBill = () => {
-    return <div></div>;
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+const AddBill = (props) => {
+    const [amount, setAmount] = useState();
+    const [category, setCategory] = useState(props.categories[0]);
+    const [date, setDate] = useState(new Date());
+
+    const handleChangeCategory = (e) => {
+        setCategory(e.target.value);
+    };
+
+    const handleChangeAmount = (e) => {
+        setAmount(parseInt(e.target.value), 10);
+    };
+
+    const handleChangeDate = (e) => {
+        setDate(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!amount) {
+            alert("Please enter an amount");
+            return;
+        }
+
+        props.onSubmit(amount, category || props.categories[0], date);
+    };
+
+    return (
+        <form className="h-100 w-full flex items-center justify-center font-sans">
+            <div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
+                <div className="mb-4">
+                    <h1 className="text-grey-darkest">Enter a new bill</h1>
+                    <p>E.g. 'Electricity' or 'Gas' or 'Internet'</p>
+                    <div className="flex mt-4">
+                        <input className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker" placeholder="Add category" value={amount} onChange={handleChangeAmount} />
+
+                        <select>
+                            {props.categories
+                                ? props.categories.map((value, index) => {
+                                      return (
+                                          <option key={index} value={value}>
+                                              {value}
+                                          </option>
+                                      );
+                                  })
+                                : ""}
+                        </select>
+
+                        <DatePicker selected={date} onChange={handleChangeDate} />
+
+                        <button className="flex-no-shrink p-2 border-2 rounded bg-white text-black border-teal hover:text-white hover:bg-black" onClick={handleSubmit}>
+                            Add
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </form>
+    );
 };
 
 export default AddBill;
--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ import BillsTable from "./components/BillsTable";
 function App() {
     const [shouldShowAddCategory, setShouldShowAddCategory] = useState(false);
     const [categories, setCategories] = useState([]);
+    const [bills, setBills] = useState([]);
+    const [shouldShowAddBill, setShouldShowAddBill] = useState(true);
 
     const addCategory = (category) => {
         const updateCategories = [...(categories || []), category];
@@ -24,14 +26,24 @@ function App() {
         setShouldShowAddCategory(true);
     };
 
-    useEffect(() => {
+    const addBill = (amount, category, date) => {
+        const bill = { amount, category, date };
+        const updatedBills = [...(bills || []), bill];
+        setBills(updatedBills);
+        setShouldShowAddBill(false);
+        localStorage.setItem("bills", JSON.stringify(updatedBills));
+    };
+
+    /* useEffect(() => {
         const categoriesInLocalStorage = JSON.parse(localStorage.getItem("categories"));
+        const billsInLocalStorage = JSON.parse(localStorage.getItem("bills"));
+
         console.log(categoriesInLocalStorage);
 
-        if (categoriesInLocalStorage !== categories) {
+       // if (categoriesInLocalStorage !== categories) {
             setCategories(categoriesInLocalStorage);
             console.log(categories);
-        }
+       // }
 
         const categoriesExistAfterInsert = JSON.stringify(localStorage.getItem("categories"));
 
@@ -39,15 +51,29 @@ function App() {
             console.log("passsei");
             setShouldShowAddCategory(false);
         }
+    }, []);*/
+
+    useEffect(() => {
+        const categoriesInLocalStorage = JSON.parse(localStorage.getItem("categories"));
+        const billsInLocalStorage = JSON.parse(localStorage.getItem("bills"));
+
+        setCategories(categoriesInLocalStorage);
+        setBills(billsInLocalStorage);
+
+        if (!categoriesInLocalStorage) {
+            setShouldShowAddCategory(true);
+        }
     }, []);
 
     return (
         <div className="App">
             {shouldShowAddCategory ? (
-                <AddCategory onSubmit={addCategory} showAddCategory={showAddCategory} />
+                <AddCategory onSubmit={addCategory} />
+            ) : shouldShowAddBill ? (
+                <AddBill onSubmit={addBill} categories={categories} />
             ) : (
                 <div>
-                    <NavBar categories={categories} />
+                    <NavBar categories={categories} showAddCategory={showAddCategory} />
                     <div className="container flex">
                         <div className="w-1/2">
                             <BillsTable />
